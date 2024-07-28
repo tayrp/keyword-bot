a6a43d849cd22ffea39a298bdf041d599c4f6ac6
tay <tay@havenmc.org>
2024-07-27 21:02:30 -0400

    Added some simple files, Dosent work.

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import keyword
+from discord import Client
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.typing = True
+intents.presences = True
+
+client = commands.Bot(command_prefix = 'tia.', intents=intents)
+
+keywords = ["You're a fat fuck", "You mother has a fat ass"]
+
+@client.event
+async def on_ready():
+    print("Tia is now online")
+    print("Searching for Keywords")
+    print("----------------------")
+
+@client.event
+async def on_message(message):
+    for t in range(len(keywords)):
+      message_text = message.content
+      if keywords in message.content:
+        if (message.content == 'coolmsg'):
+            await client.send_message(message.channel, "'{}' was said".format(keyword))
